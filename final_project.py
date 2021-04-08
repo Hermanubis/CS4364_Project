b142e5670f64207cc906b41b0adbd9812a9045c7
Alexander Li <43350164+Hermanubis@users.noreply.github.com>
2021-04-08 15:46:20 -0400

    update

--- final_project.py
+++ final_project.py
@@ -2,14 +2,15 @@ import numpy as np
 import pandas as pd
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 
-fifaDS =  pd.read_csv('FIFA_Statistics.csv', na_values="not available")
-playoffDS =  pd.read_csv('FIFA_Statistics.csv', na_values="not available")
+fifaDS =  pd.read_csv('FIFA18_Statistics.csv', na_values="not available")
+playoffDS =  pd.read_csv('FIFA18_Statistics.csv', na_values="not available")
 
 fifaDS.drop(fifaDS.tail(16).index, inplace = True)
 y = (fifaDS['Win'] == "Yes")
@@ -20,6 +21,15 @@ numeric = [i for i in inputs.columns if inputs[i].dtype in [np.int64]]
 # X = inputs.iloc[:, 1:]
 X = inputs[numeric]
 
+playoffDS.drop(playoffDS.head(112).index, inplace = True)
+y_test = (fifaDS['Win'] == "Yes")
+# print(playoffDS)
+test_inputs = playoffDS.drop('Win', axis = 'columns')
+test_inputs = test_inputs.drop('Man of the Match', axis = 'columns')
+
+numeric_y = [i for i in test_inputs.columns if test_inputs[i].dtype in [np.int64]]
+# X = inputs.iloc[:, 1:]
+x_test = test_inputs[numeric_y]
 
 # le_team = LabelEncoder()
 
@@ -44,3 +54,4 @@ for i in range(len(featureImportance2)):
 y_pred2 = RandomForest.predict(X_test)
 randomForestScore = accuracy_score(y_test, y_pred2)
 print("Accuracy Score: " + str(randomForestScore))
+

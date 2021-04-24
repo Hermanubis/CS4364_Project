050f5e6ce601b26a39cbcae2828644b06a4d7c9c
wxyfreedom <wxyowen@gmail.com>
2021-04-23 23:18:46 -0400

    group data processing done?

--- final_project.py
+++ final_project.py
@@ -12,20 +12,22 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import OneHotEncoder
 
-def group_stage_proc(group_data, fifaGroupDS):
-    # Dropping unnecessary columns    
-    group_data = group_data.drop(['Date', 'Man of the Match', 'Round', 'PSO', 'Goals in PSO', 'Own goals'], axis = 'columns')
-    
+def group_stage_proc(group_data, fifaGroupDS):      
     group_data = group_data.groupby('Team').sum()
+    group_data = pd.merge(group_data, fifaGroupDS, on='Team')
+    
+    # Dropping unnecessary columns    
+    group_data = group_data.drop(['Goals in PSO', 'Own goals', 'Group', 'GroupWin', 
+                                  'GroupDraw', 'GroupLost', 'GroupScore'], axis = 'columns')
     
     # pd.set_option("display.max_rows", None, "display.max_columns", None)
     
-    y = fifaGroupDS['Advance']
+    y = group_data['Advance']
     
     numeric = [i for i in group_data.columns if group_data[i].dtype in [np.int64]]
     X = group_data[numeric]
     
-    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.25,random_state=0)
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=0)
 
     sc = StandardScaler()
     X_train = sc.fit_transform(X_train)
@@ -45,53 +47,25 @@ def group_stage_proc(group_data, fifaGroupDS):
         fea_importance_ranked.append((X.columns[i], featureImportance2[i]))
     
     fea_importance_ranked.sort(key=lambda x:x[1], reverse=True)
-    
-    print(fea_importance_ranked)
+    for tup in fea_importance_ranked:
+        print("Feature: " + str(tup[0]) + " --- Importance: " + str(tup[1]))
     
     y_pred = RandomForest.predict(X_test)
     randomForestScore = accuracy_score(y_test, y_pred)
     print("Accuracy Score: " + str(randomForestScore))
     
-    model = LogisticRegression(max_iter = 50000)
-    model.fit(X_train, y_train)
-    trainScore = model.score(X_train, y_train)
-    testScore = model.score(X_test, y_test)
+    # model = LogisticRegression(max_iter = 50000)
+    # model.fit(X_train, y_train)
+    # trainScore = model.score(X_train, y_train)
+    # testScore = model.score(X_test, y_test)
     
-    # y_pred = model.predict(test_inputs)
-    # playoffScore = accuracy_score(y_test, y_pred)
+    # # y_pred = model.predict(test_inputs)
+    # # playoffScore = accuracy_score(y_test, y_pred)
     
-    print("\nLogistic Regression: ")
-    print("Train Score: " + str(trainScore))
-    print("Test Score: " + ": " + str(testScore))
+    # print("\nLogistic Regression: ")
+    # print("Train Score: " + str(trainScore))
+    # print("Test Score: " + ": " + str(testScore))
     
-
-# def group_stage(inputs):
-#     # Only grabbing data from the group stage at this point    
-#     inputs = inputs[inputs['Round'] == 'Group Stage']
-    
-#     # Dropping unnecessary columns    
-#     inputs = inputs.drop(['Date', 'Man of the Match', 'Round', 'PSO', 'Goals in PSO', 'Own goals'], axis = 'columns')
-    
-#     RandomForest = RandomForestClassifier(criterion="entropy")
-#     RandomForest.fit(X_train, y_train)
-#     featureImportance2 = RandomForest.feature_importances_
-    
-#     # Rank features by their importance
-#     rankedFeatures = []
-    
-#     print("\nRandom Forest: ")
-#     for i in range(len(featureImportance2)):
-#         rankedFeatures.append((X.columns[i], featureImportance2[i]))
-        
-#     rankedFeatures.sort(key=lambda x:x[1], reverse=True)
-    
-#     for tup in rankedFeatures:
-#         print("Feature: " + str(tup[0]) + " --- Importance: " + str(tup[1]))
-        
-#     y_pred = RandomForest.predict(X_test)
-#     randomForestScore = accuracy_score(y_test, y_pred)
-#     print("Accuracy Score: " + str(randomForestScore))
-
 
 fifaDS =  pd.read_csv('FIFA18_Statistics.csv', na_values="not available")
 playoffDS =  pd.read_csv('FIFA18_Statistics.csv', na_values="not available")
